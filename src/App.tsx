3ed2c5d435ca28de52521a602088833ec62ea22c
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-25 02:16:25 -0300

    feat: :sparkles: Add Video and Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Event } from './pages/Event';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+import { client } from './lib/apollo';
 
 
 export default function App() {
 
   return (
-    <>
-      <Event/>
-    </>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
