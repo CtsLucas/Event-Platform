86c9a8e94bdee3003c58bb9e13af6ad1f08fc142
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-25 04:18:34 -0300

    feat: :sparkles: Add Subscribe

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -44,16 +44,16 @@ export function Video( props: VideoProps ) {
 
   if(!data) {
     return (
-      <div className='flex-1'>
+      <div className="flex-1">
         <p>Carregando...</p>
       </div>
     )
   }
 
   return(
-    <div className='flex-1'>
-      <div className='bg-black flex justify-center'>
-        <div className='h-full w-full max-w-[1100px] man-h-[60vh] aspect-video'>
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] man-h-[60vh] aspect-video">
           <Player>
             <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
@@ -62,26 +62,26 @@ export function Video( props: VideoProps ) {
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
-        <div className='flex items-start gap-16'>
-          <div className='flex-1'>
-            <h1 className='text-2xl font-bold'>
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
             {data.lesson.title}
             </h1>
-            <p className='mt-4 text-gray-200 leading-relaxed'>
+            <p className="mt-4 text-gray-200 leading-relaxed">
             {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img 
-                className='h-16 w-16 rounded-full border-2 border-blue-500'
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
                 src={data.lesson.teacher.avatarURL} 
                 alt="" />
 
-              <div className='leading-relaxed'>
-                <strong className='font-bold text-2xl block'>
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">
                   {data.lesson.teacher.name}
                 </strong>
-                <span className='text-gray-200 text-sm block'>
+                <span className="text-gray-200 text-sm block">
                   {data.lesson.teacher.bio}
                 </span>
               </div> 
